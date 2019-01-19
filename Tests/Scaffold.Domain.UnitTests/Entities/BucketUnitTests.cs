9f9cc7ed1522d04c3a7d1e6ede7635218c26e722
Hannah Chan <hannah.chan@outlook.com>
2019-01-19 22:30:10 +1100

    Added some unit tests.

--- Tests/Scaffold.Domain.UnitTests/Entities/BucketUnitTests.cs
+++ Tests/Scaffold.Domain.UnitTests/Entities/BucketUnitTests.cs
@@ -1,5 +1,7 @@
 namespace Scaffold.Domain.UnitTests.Entities
 {
+    using System;
+    using Scaffold.Domain.Entities;
     using Xunit;
 
     public class BucketUnitTests
@@ -9,16 +11,45 @@ namespace Scaffold.Domain.UnitTests.Entities
             [Fact]
             public void When_AddingItem_Expect_ItemAdded()
             {
+                // Arrange
+                Bucket bucket = new Bucket();
+                Item item = new Item();
+
+                // Act
+                bucket.AddItem(item);
+
+                // Assert
+                Assert.Contains(item, bucket.Items);
+                Assert.Equal(bucket, item.Bucket);
             }
 
             [Fact]
             public void When_AddingItemMoreThanOnce_Expect_ItemAddedOnlyOnce()
             {
+                // Arrange
+                Bucket bucket = new Bucket();
+                Item item = new Item();
+
+                // Act
+                bucket.AddItem(item);
+                bucket.AddItem(item);
+
+                // Assert
+                Assert.Equal(1, bucket.Items.Count);
             }
 
             [Fact]
             public void When_AddingNull_Expect_ArgumentNullException()
             {
+                // Arrange
+                Bucket bucket = new Bucket();
+
+                // Act
+                Action action = () => bucket.AddItem(null);
+
+                // Assert
+                Assert.Throws<ArgumentNullException>(action);
+                Assert.Empty(bucket.Items);
             }
         }
 
@@ -27,16 +58,54 @@ namespace Scaffold.Domain.UnitTests.Entities
             [Fact]
             public void When_RemovingItem_Expect_ItemRemoved()
             {
+                // Arrange
+                Bucket bucket = new Bucket();
+                Item item = new Item();
+
+                bucket.AddItem(item);
+
+                // Act
+                bucket.RemoveItem(item);
+
+                // Assert
+                Assert.DoesNotContain(item, bucket.Items);
+                Assert.Null(item.Bucket);
             }
 
             [Fact]
             public void When_RemovingItemNotInBucket_Expect_NoChange()
             {
+                // Arrange
+                Bucket bucket = new Bucket();
+                Item item = new Item();
+
+                bucket.AddItem(item);
+
+                // Act
+                bucket.RemoveItem(new Item());
+
+                // Assert
+                Assert.Contains(item, bucket.Items);
+                Assert.Equal(1, bucket.Items.Count);
+                Assert.Equal(bucket, item.Bucket);
             }
 
             [Fact]
             public void When_RemovingNull_Expect_NoChange()
             {
+                // Arrange
+                Bucket bucket = new Bucket();
+                Item item = new Item();
+
+                bucket.AddItem(item);
+
+                // Act
+                bucket.RemoveItem(null);
+
+                // Assert
+                Assert.Contains(item, bucket.Items);
+                Assert.Equal(1, bucket.Items.Count);
+                Assert.Equal(bucket, item.Bucket);
             }
         }
     }
