e95d481ed206a390ea7ba268a082a4de54e0e6f0
Hannah Chan <hannah.chan@outlook.com>
2019-01-21 20:09:37 +1100

    Scaffold Controllers.

---
+++ Sources/Scaffold.WebApi/Controllers/BucketsController.cs
@@ -0,0 +1,37 @@
+﻿namespace Scaffold.WebApi.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BucketsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Bucket 1", "Bucket 2" };
+        }
+
+        [HttpGet("{bucketId}")]
+        public ActionResult<string> Get(int bucketId)
+        {
+            return $"Bucket {bucketId}";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPatch("{bucketId}")]
+        public void Patch(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{bucketId}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Sources/Scaffold.WebApi/Controllers/ItemsController.cs
@@ -0,0 +1,37 @@
+﻿namespace Scaffold.WebApi.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/buckets/{bucketId}/[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Item 1", "Item 2" };
+        }
+
+        [HttpGet("{itemId}")]
+        public ActionResult<string> Get(int itemId)
+        {
+            return $"Item {itemId}";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPatch("{itemId}")]
+        public void Patch(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{itemId}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
