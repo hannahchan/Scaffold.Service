95d3de964615b496e1dbb4ebeeefd1b462a66e71
Hannah Chan <hannah.chan@outlook.com>
2019-01-23 19:43:13 +1100

    Implemented Get Buckets.

--- Sources/Scaffold.Application/Features/Bucket/GetBuckets.cs
+++ Sources/Scaffold.Application/Features/Bucket/GetBuckets.cs
@@ -1,6 +1,32 @@
 namespace Scaffold.Application.Features.Bucket
 {
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using MediatR;
+    using Scaffold.Application.Context;
+    using Scaffold.Application.Repositories;
+    using Scaffold.Domain.Entities;
+
     public class GetBuckets
     {
+        public class Query : ApplicationRequest, IRequest<Response>
+        {
+        }
+
+        public class Response : ApplicationResponse
+        {
+            public IList<Bucket> Buckets { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Response>
+        {
+            private readonly IBucketRepository repository;
+
+            public Handler(IBucketRepository repository) => this.repository = repository;
+
+            public async Task<Response> Handle(Query query, CancellationToken cancellationToken) =>
+                new Response { Buckets = await this.repository.GetAllAsync() };
+        }
     }
 }
---
+++ Tests/Scaffold.Application.UnitTests/Features/Bucket/GetBucketsUnitTests.cs
@@ -0,0 +1,64 @@
+namespace Scaffold.Application.UnitTests.Features.Bucket
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using Scaffold.Application.Features.Bucket;
+    using Scaffold.Application.Repositories;
+    using Scaffold.Data;
+    using Scaffold.Domain.Entities;
+    using Xunit;
+
+    public class GetBucketsUnitTests
+    {
+        private readonly IBucketRepository repository;
+
+        public GetBucketsUnitTests()
+        {
+            BucketContext context = new BucketContext(new DbContextOptionsBuilder<BucketContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options);
+
+            this.repository = new BucketRepository(context);
+        }
+
+        public class Handler : GetBucketsUnitTests
+        {
+            [Fact]
+            public async Task When_GettingExistingBuckets_Expect_ExistingBuckets()
+            {
+                // Arrange
+                this.repository.Add(new Bucket());
+                this.repository.Add(new Bucket());
+                this.repository.Add(new Bucket());
+
+                GetBuckets.Query query = new GetBuckets.Query();
+                GetBuckets.Handler handler = new GetBuckets.Handler(this.repository);
+
+                // Act
+                GetBuckets.Response response = await handler.Handle(query, default(CancellationToken));
+
+                // Assert
+                Assert.NotNull(response.Buckets);
+                Assert.NotEmpty(response.Buckets);
+                Assert.Equal(3, response.Buckets.Count);
+            }
+
+            [Fact]
+            public async Task When_GettingNonExistingBuckets_Expect_EmptyList()
+            {
+                // Arrange
+                GetBuckets.Query query = new GetBuckets.Query();
+                GetBuckets.Handler handler = new GetBuckets.Handler(this.repository);
+
+                // Act
+                GetBuckets.Response response = await handler.Handle(query, default(CancellationToken));
+
+                // Assert
+                Assert.NotNull(response.Buckets);
+                Assert.Empty(response.Buckets);
+            }
+        }
+    }
+}
