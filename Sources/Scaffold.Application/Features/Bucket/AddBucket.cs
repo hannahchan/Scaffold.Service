c225370d75bfdfcc1e7d8be3ee18866ce3db875d
Hannah Chan <hannah.chan@outlook.com>
2019-01-23 19:03:59 +1100

    Implemented Add Bucket.

--- Sources/Scaffold.Application/Features/Bucket/AddBucket.cs
+++ Sources/Scaffold.Application/Features/Bucket/AddBucket.cs
@@ -1,6 +1,58 @@
 namespace Scaffold.Application.Features.Bucket
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using FluentValidation;
+    using MediatR;
+    using Scaffold.Application.Context;
+    using Scaffold.Application.Repositories;
+    using Scaffold.Domain.Entities;
+
     public class AddBucket
     {
+        public class Command : ApplicationRequest, IRequest<Response>
+        {
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+        }
+
+        public class Response : ApplicationResponse
+        {
+            public Bucket Bucket { get; set; }
+        }
+
+        public class Validator : AbstractValidator<Command>
+        {
+            public Validator()
+            {
+                this.RuleFor(command => command.Name).NotEmpty().NotNull();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Response>
+        {
+            private readonly IBucketRepository repository;
+
+            public Handler(IBucketRepository repository) => this.repository = repository;
+
+            public async Task<Response> Handle(Command command, CancellationToken cancellationToken)
+            {
+                await new Validator().ValidateAndThrowAsync(command);
+
+                Response response = new Response
+                {
+                    Bucket = new Bucket
+                    {
+                        Name = command.Name,
+                        Description = command.Description
+                    }
+                };
+
+                await this.repository.AddAsync(response.Bucket);
+
+                return response;
+            }
+        }
     }
 }
---
+++ Tests/Scaffold.Application.UnitTests/Features/AddBucketUnitTests.cs
@@ -0,0 +1,92 @@
+namespace Scaffold.Application.UnitTests.Features
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using FluentValidation;
+    using FluentValidation.TestHelper;
+    using Microsoft.EntityFrameworkCore;
+    using Scaffold.Application.Features.Bucket;
+    using Scaffold.Application.Repositories;
+    using Scaffold.Data;
+    using Xunit;
+
+    public class AddBucketUnitTests
+    {
+        private readonly IBucketRepository repository;
+
+        public AddBucketUnitTests()
+        {
+            BucketContext modernContext = new BucketContext(new DbContextOptionsBuilder<BucketContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options);
+
+            this.repository = new BucketRepository(modernContext);
+        }
+
+        public class Validator
+        {
+            [Fact]
+            public void ShouldNotHaveValidationErrorFor()
+            {
+                // Arrange
+                AddBucket.Validator validator = new AddBucket.Validator();
+
+                // Act and Assert
+                validator.ShouldNotHaveValidationErrorFor(command => command.Name, Guid.NewGuid().ToString());
+                validator.ShouldNotHaveValidationErrorFor(command => command.Description, Guid.NewGuid().ToString());
+            }
+
+            [Fact]
+            public void ShouldHaveValidationErrorFor()
+            {
+                // Arrange
+                AddBucket.Validator validator = new AddBucket.Validator();
+
+                // Act and Assert
+                validator.ShouldHaveValidationErrorFor(command => command.Name, string.Empty);
+                validator.ShouldHaveValidationErrorFor(command => command.Name, null as string);
+            }
+        }
+
+        public class Handler : AddBucketUnitTests
+        {
+            [Fact]
+            public async Task When_AddingNewBucket_Expect_AddedBucket()
+            {
+                // Arrange
+                AddBucket.Command command = new AddBucket.Command
+                {
+                    Name = Guid.NewGuid().ToString(),
+                    Description = Guid.NewGuid().ToString()
+                };
+
+                AddBucket.Handler handler = new AddBucket.Handler(this.repository);
+
+                // Act
+                AddBucket.Response response = await handler.Handle(command, default(CancellationToken));
+
+                // Assert
+                Assert.NotEqual(default(int), response.Bucket.Id);
+                Assert.Equal(command.Name, response.Bucket.Name);
+                Assert.NotNull(response.Bucket.Items);
+            }
+
+            [Fact]
+            public async Task When_AddingNewBucketWithInvalidCommand_Expect_ValidationException()
+            {
+                // Arrange
+                AddBucket.Command command = new AddBucket.Command { Name = string.Empty };
+                AddBucket.Handler handler = new AddBucket.Handler(this.repository);
+
+                // Act
+                Exception exception = await Record.ExceptionAsync(() =>
+                    handler.Handle(command, default(CancellationToken)));
+
+                // Assert
+                Assert.NotNull(exception);
+                Assert.IsType<ValidationException>(exception);
+            }
+        }
+    }
+}
