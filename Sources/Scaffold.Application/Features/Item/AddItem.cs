1bf25f765700a21f3ee86979514e5b67cb4d5ca8
Hannah Chan <hannah.chan@outlook.com>
2019-01-28 13:54:14 +1100

    Revised mapping logic for AddItem.cs.

--- Sources/Scaffold.Application/Features/Item/AddItem.cs
+++ Sources/Scaffold.Application/Features/Item/AddItem.cs
@@ -2,12 +2,14 @@ namespace Scaffold.Application.Features.Item
 {
     using System.Threading;
     using System.Threading.Tasks;
+    using AutoMapper;
     using FluentValidation;
     using MediatR;
     using Scaffold.Application.Context;
     using Scaffold.Application.Exceptions;
     using Scaffold.Application.Repositories;
     using Scaffold.Domain.Entities;
+    using Scaffold.Domain.Exceptions;
 
     public class AddItem
     {
@@ -46,20 +48,34 @@ namespace Scaffold.Application.Features.Item
                 Bucket bucket = await this.repository.GetAsync(command.BucketId) ??
                     throw new BucketNotFoundException(command.BucketId);
 
-                Response response = new Response
+                Response response = new Response();
+
+                try
                 {
-                    Item = new Item
-                    {
-                        Name = command.Name,
-                        Description = command.Description
-                    }
-                };
+                    MapperConfiguration configuration = new MapperConfiguration(config => config.AddProfile(new MappingProfile()));
+                    response.Item = configuration.CreateMapper().Map<Item>(command);
+                }
+                catch (AutoMapperMappingException exception) when (exception.InnerException is DomainException)
+                {
+                    throw exception.InnerException;
+                }
 
                 bucket.AddItem(response.Item);
 
                 await this.repository.UpdateAsync(bucket);
 
                 return response;
+            }
+        }
+
+        public class MappingProfile : Profile
+        {
+            public MappingProfile()
+            {
+                this.CreateMap<Command, Item>()
+                    .AddTransform<string>(value => value == string.Empty ? null : value)
+                    .ForMember(dest => dest.Id, opt => opt.Ignore())
+                    .ForMember(dest => dest.Bucket, opt => opt.Ignore());
             }
         }
     }
--- Tests/Scaffold.Application.UnitTests/Features/Item/AddItemUnitTests.cs
+++ Tests/Scaffold.Application.UnitTests/Features/Item/AddItemUnitTests.cs
@@ -3,6 +3,7 @@ namespace Scaffold.Application.UnitTests.Features.Item
     using System;
     using System.Threading;
     using System.Threading.Tasks;
+    using AutoMapper;
     using FluentValidation;
     using FluentValidation.TestHelper;
     using Microsoft.EntityFrameworkCore;
@@ -138,6 +139,38 @@ namespace Scaffold.Application.UnitTests.Features.Item
                 // Assert
                 Assert.NotNull(exception);
                 Assert.IsType<BucketFullException>(exception);
+            }
+        }
+
+        public class MappingProfile
+        {
+            [Fact]
+            public void IsValid()
+            {
+                // Arrange
+                AddItem.MappingProfile profile = new AddItem.MappingProfile();
+                MapperConfiguration configuration = new MapperConfiguration(config => config.AddProfile(profile));
+
+                // Act and Assert
+                configuration.AssertConfigurationIsValid();
+            }
+
+            [Fact]
+            public void When_MappingCommandToBucketWithEmptyStringProperties_Expect_NullMappedToStringProperties()
+            {
+                // Arrange
+                Item item = new Item { Name = "abc", Description = "xyz" };
+                AddItem.Command command = new AddItem.Command { Name = string.Empty, Description = string.Empty };
+
+                MapperConfiguration configuration = new MapperConfiguration(config =>
+                    config.AddProfile(new AddItem.MappingProfile()));
+
+                // Act
+                item = configuration.CreateMapper().Map<AddItem.Command, Item>(command, item);
+
+                // Assert
+                Assert.Null(item.Name);
+                Assert.Null(item.Description);
             }
         }
     }
