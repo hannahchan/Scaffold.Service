0da376e17ca1e7353bb7b0c292c01237ffe77d05
Hannah Chan <hannah.chan@outlook.com>
2019-08-06 00:40:42 +1000

    Revised ExceptionFilter

--- Scaffold/Sources/Scaffold.WebApi/Filters/ExceptionFilter.cs
+++ Scaffold/Sources/Scaffold.WebApi/Filters/ExceptionFilter.cs
@@ -25,25 +25,34 @@ namespace Scaffold.WebApi.Filters
 
         public void OnException(ExceptionContext context)
         {
+            ProblemDetails details = null;
+
             if (context.Exception is DomainException domainException)
             {
-                ProblemDetails details = this.GetProblemDetails(domainException);
-                details.Extensions[this.ToCamelCase(CustomHeaderNames.RequestId)] = context.HttpContext.TraceIdentifier;
+                details = this.GetProblemDetails(domainException);
                 context.Result = new ConflictObjectResult(details) { ContentTypes = this.contentTypes };
             }
 
             if (context.Exception is NotFoundException notFoundException)
             {
-                ProblemDetails details = this.GetProblemDetails(notFoundException);
-                details.Extensions[this.ToCamelCase(CustomHeaderNames.RequestId)] = context.HttpContext.TraceIdentifier;
+                details = this.GetProblemDetails(notFoundException);
                 context.Result = new NotFoundObjectResult(details) { ContentTypes = this.contentTypes };
             }
 
             if (context.Exception is ValidationException validationException)
             {
-                ValidationProblemDetails details = this.GetProblemDetails(validationException);
-                details.Extensions[this.ToCamelCase(CustomHeaderNames.RequestId)] = context.HttpContext.TraceIdentifier;
+                details = this.GetProblemDetails(validationException);
                 context.Result = new BadRequestObjectResult(details) { ContentTypes = this.contentTypes };
+            }
+
+            if (details != null)
+            {
+                if (!string.IsNullOrEmpty(this.tracingService?.CorrelationId))
+                {
+                    details.Extensions[this.ToCamelCase(CustomHeaderNames.CorrelationId)] = this.tracingService.CorrelationId;
+                }
+
+                details.Extensions[this.ToCamelCase(CustomHeaderNames.RequestId)] = context.HttpContext.TraceIdentifier;
             }
         }
 
@@ -54,11 +63,6 @@ namespace Scaffold.WebApi.Filters
                 Title = exception.Title,
                 Detail = exception.Detail,
             };
-
-            if (!string.IsNullOrEmpty(this.tracingService?.CorrelationId))
-            {
-                details.Extensions[this.ToCamelCase(CustomHeaderNames.CorrelationId)] = this.tracingService.CorrelationId;
-            }
 
             return details;
         }
@@ -71,22 +75,12 @@ namespace Scaffold.WebApi.Filters
                 Detail = exception.Detail,
             };
 
-            if (!string.IsNullOrEmpty(this.tracingService?.CorrelationId))
-            {
-                details.Extensions[this.ToCamelCase(CustomHeaderNames.CorrelationId)] = this.tracingService.CorrelationId;
-            }
-
             return details;
         }
 
         private ValidationProblemDetails GetProblemDetails(ValidationException exception)
         {
             ValidationProblemDetails details = new ValidationProblemDetails { Title = "Validation Failure" };
-
-            if (!string.IsNullOrEmpty(this.tracingService?.CorrelationId))
-            {
-                details.Extensions[this.ToCamelCase(CustomHeaderNames.CorrelationId)] = this.tracingService.CorrelationId;
-            }
 
             foreach (string property in exception.Errors.Select(error => error.PropertyName).Distinct())
             {
