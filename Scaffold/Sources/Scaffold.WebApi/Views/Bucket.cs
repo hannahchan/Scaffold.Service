28fc9d15de8f9656742f21b962a5aa5ea960b3bf
Hannah Chan <hannah.chan@outlook.com>
2020-06-04 22:08:35 +1000

    Revised nullability

--- Scaffold/Sources/Scaffold.WebApi/Views/Bucket.cs
+++ Scaffold/Sources/Scaffold.WebApi/Views/Bucket.cs
@@ -5,7 +5,7 @@ namespace Scaffold.WebApi.Views
     public class Bucket
     {
         [BindNever]
-        public int? Id { get; set; }
+        public int Id { get; set; }
 
         public string? Name { get; set; }
 
--- Scaffold/Sources/Scaffold.WebApi/Views/Item.cs
+++ Scaffold/Sources/Scaffold.WebApi/Views/Item.cs
@@ -5,7 +5,7 @@ namespace Scaffold.WebApi.Views
     public class Item
     {
         [BindNever]
-        public int? Id { get; set; }
+        public int Id { get; set; }
 
         public string? Name { get; set; }
 
