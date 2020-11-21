43b3b8cd9df69d8b2565f43b594537b194bac07d
Hannah Chan <hannah.chan@outlook.com>
2020-11-21 18:30:13 +1100

    Add Data Annotations to correct Swagger output

--- Scaffold/Sources/Scaffold.WebApi/Views/Bucket/Bucket.cs
+++ Scaffold/Sources/Scaffold.WebApi/Views/Bucket/Bucket.cs
@@ -1,13 +1,17 @@
 namespace Scaffold.WebApi.Views.Bucket
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Bucket
     {
+        [Required]
         public int Id { get; set; }
 
         public string? Name { get; set; }
 
         public string? Description { get; set; }
 
+        [Required]
         public int Size { get; set; }
     }
 }
--- Scaffold/Sources/Scaffold.WebApi/Views/Item/Item.cs
+++ Scaffold/Sources/Scaffold.WebApi/Views/Item/Item.cs
@@ -1,7 +1,10 @@
 namespace Scaffold.WebApi.Views.Item
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Item
     {
+        [Required]
         public int Id { get; set; }
 
         public string? Name { get; set; }
