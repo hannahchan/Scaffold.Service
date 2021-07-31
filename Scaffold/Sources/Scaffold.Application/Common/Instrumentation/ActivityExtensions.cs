5d295a85ff550e6092eee2ec60e23548830446d4
Hannah Chan <hannah.chan@outlook.com>
2021-07-31 12:34:02 +1000

    Add Activity Extensions

--- Scaffold/Sources/Scaffold.Application/Common/Instrumentation/ActivityExtensions.cs
+++ Scaffold/Sources/Scaffold.Application/Common/Instrumentation/ActivityExtensions.cs
@@ -5,6 +5,36 @@ namespace Scaffold.Application.Common.Instrumentation
 
     internal static class ActivityExtensions
     {
+        public static string? GetSpanId(this Activity activity)
+        {
+            return activity.IdFormat switch
+            {
+                ActivityIdFormat.Hierarchical => activity.Id,
+                ActivityIdFormat.W3C => activity.SpanId.ToHexString(),
+                _ => null,
+            };
+        }
+
+        public static string? GetTraceId(this Activity activity)
+        {
+            return activity.IdFormat switch
+            {
+                ActivityIdFormat.Hierarchical => activity.RootId,
+                ActivityIdFormat.W3C => activity.TraceId.ToHexString(),
+                _ => null,
+            };
+        }
+
+        public static string? GetParentId(this Activity activity)
+        {
+            return activity.IdFormat switch
+            {
+                ActivityIdFormat.Hierarchical => activity.ParentId,
+                ActivityIdFormat.W3C => activity.ParentSpanId.ToHexString(),
+                _ => null,
+            };
+        }
+
         public static Activity? InvokeIfNotNullAndIsAllDataRequested(this Activity? activity, Action<Activity> action)
         {
             if (activity != null && activity.IsAllDataRequested)
---
+++ Scaffold/Tests/UnitTests/Scaffold.Application.UnitTests/TestCollection.cs
@@ -0,0 +1,7 @@
+namespace Scaffold.Application.UnitTests
+{
+    public static class TestCollection
+    {
+        public const string Activity = nameof(Activity);
+    }
+}
