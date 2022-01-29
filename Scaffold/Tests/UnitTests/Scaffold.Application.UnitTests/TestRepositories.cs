2b211f859fc3184c5a167aa05140618615d7b5e3
Hannah Chan <hannah.chan@outlook.com>
2022-01-29 21:36:16 +1100

    Use DbContextFactory

--- Scaffold/Tests/UnitTests/Scaffold.Application.UnitTests/TestRepositories.cs
+++ Scaffold/Tests/UnitTests/Scaffold.Application.UnitTests/TestRepositories.cs
@@ -4,21 +4,45 @@ namespace Scaffold.Application.UnitTests;
 
 using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Scaffold.Application.Components.Bucket;
 using Scaffold.Repositories;
 using Xunit;
 
-public class TestRepositories : TheoryData<IBucketRepository>
+public sealed class TestRepositories : TheoryData<IBucketRepository>, IDisposable
 {
+    private readonly ServiceProvider serviceProvider = CreateServiceProvider();
+
+    private bool disposed;
+
     public TestRepositories()
     {
-        this.Add(new ScopedBucketRepository(CreateNewBucketContext()));
+        this.Add(new ScopedBucketRepository(this.CreateNewBucketContext()));
     }
 
-    private static BucketContext CreateNewBucketContext()
+    public void Dispose()
     {
-        return new BucketContext(new DbContextOptionsBuilder<BucketContext>()
-            .UseInMemoryDatabase(Guid.NewGuid().ToString())
-            .Options);
+        if (this.disposed)
+        {
+            return;
+        }
+
+        this.disposed = true;
+        this.serviceProvider?.Dispose();
+    }
+
+    private static ServiceProvider CreateServiceProvider()
+    {
+        ServiceCollection services = new ServiceCollection();
+        services.AddDbContextFactory<BucketContext>(builder => builder.UseInMemoryDatabase(Guid.NewGuid().ToString()));
+
+        return services.BuildServiceProvider();
+    }
+
+    private BucketContext CreateNewBucketContext()
+    {
+        return this.serviceProvider
+            .GetRequiredService<IDbContextFactory<BucketContext>>()
+            .CreateDbContext();
     }
 }
