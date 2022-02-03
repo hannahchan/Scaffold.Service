c0fbc0a5d62e66c0e6c11df12ca70c3bc07336d0
Hannah Chan <hannah.chan@outlook.com>
2022-02-03 23:11:21 +1100

    Draft

--- Scaffold/Sources/Adapters/Scaffold.Repositories/SingletonBucketRepository.cs
+++ Scaffold/Sources/Adapters/Scaffold.Repositories/SingletonBucketRepository.cs
@@ -3,88 +3,113 @@ namespace Scaffold.Repositories;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Scaffold.Application.Components.Bucket;
 using Scaffold.Domain.Aggregates.Bucket;
 
 public class SingletonBucketRepository : SingletonBucketReadRepository, IBucketRepository
 {
-    private readonly BucketContext context;
+    private readonly IDbContextFactory<BucketContext> factory;
 
-    public SingletonBucketRepository(BucketContext context)
-        : base(context)
+    public SingletonBucketRepository(IDbContextFactory<BucketContext> factory)
+        : base(factory.CreateDbContext())
     {
-        this.context = context;
+        this.factory = factory;
     }
 
     public void Add(Bucket bucket)
     {
-        this.context.Buckets.Add(bucket);
-        this.context.SaveChanges();
+        using BucketContext context = this.factory.CreateDbContext();
+
+        context.Buckets.Add(bucket);
+        context.SaveChanges();
     }
 
     public void Add(IEnumerable<Bucket> buckets)
     {
-        this.context.Buckets.AddRange(buckets);
-        this.context.SaveChanges();
+        using BucketContext context = this.factory.CreateDbContext();
+
+        context.Buckets.AddRange(buckets);
+        context.SaveChanges();
     }
 
     public Task AddAsync(Bucket bucket, CancellationToken cancellationToken = default)
     {
-        this.context.Buckets.Add(bucket);
-        return this.context.SaveChangesAsync(cancellationToken);
+        using BucketContext context = this.factory.CreateDbContext();
+
+        context.Buckets.Add(bucket);
+        return context.SaveChangesAsync(cancellationToken);
     }
 
     public Task AddAsync(IEnumerable<Bucket> buckets, CancellationToken cancellationToken = default)
     {
-        this.context.Buckets.AddRange(buckets);
-        return this.context.SaveChangesAsync(cancellationToken);
+        using BucketContext context = this.factory.CreateDbContext();
+
+        context.Buckets.AddRange(buckets);
+        return context.SaveChangesAsync(cancellationToken);
     }
 
     public void Remove(Bucket bucket)
     {
-        this.context.Buckets.Remove(bucket);
-        this.context.SaveChanges();
+        using BucketContext context = this.factory.CreateDbContext();
+
+        context.Buckets.Remove(bucket);
+        context.SaveChanges();
     }
 
     public void Remove(IEnumerable<Bucket> buckets)
     {
-        this.context.Buckets.RemoveRange(buckets);
-        this.context.SaveChanges();
+        using BucketContext context = this.factory.CreateDbContext();
+
+        context.Buckets.RemoveRange(buckets);
+        context.SaveChanges();
     }
 
     public Task RemoveAsync(Bucket bucket, CancellationToken cancellationToken = default)
     {
-        this.context.Buckets.Remove(bucket);
-        return this.context.SaveChangesAsync(cancellationToken);
+        using BucketContext context = this.factory.CreateDbContext();
+
+        context.Buckets.Remove(bucket);
+        return context.SaveChangesAsync(cancellationToken);
     }
 
     public Task RemoveAsync(IEnumerable<Bucket> buckets, CancellationToken cancellationToken = default)
     {
-        this.context.Buckets.RemoveRange(buckets);
-        return this.context.SaveChangesAsync(cancellationToken);
+        using BucketContext context = this.factory.CreateDbContext();
+
+        context.Buckets.RemoveRange(buckets);
+        return context.SaveChangesAsync(cancellationToken);
     }
 
     public void Update(Bucket bucket)
     {
-        this.context.Buckets.Update(bucket);
-        this.context.SaveChanges();
+        using BucketContext context = this.factory.CreateDbContext();
+
+        context.Buckets.Update(bucket);
+        context.SaveChanges();
     }
 
     public void Update(IEnumerable<Bucket> buckets)
     {
-        this.context.Buckets.UpdateRange(buckets);
-        this.context.SaveChanges();
+        using BucketContext context = this.factory.CreateDbContext();
+
+        context.Buckets.UpdateRange(buckets);
+        context.SaveChanges();
     }
 
     public Task UpdateAsync(Bucket bucket, CancellationToken cancellationToken = default)
     {
-        this.context.Buckets.Update(bucket);
-        return this.context.SaveChangesAsync(cancellationToken);
+        using BucketContext context = this.factory.CreateDbContext();
+
+        context.Buckets.Update(bucket);
+        return context.SaveChangesAsync(cancellationToken);
     }
 
     public Task UpdateAsync(IEnumerable<Bucket> buckets, CancellationToken cancellationToken = default)
     {
-        this.context.Buckets.UpdateRange(buckets);
-        return this.context.SaveChangesAsync(cancellationToken);
+        using BucketContext context = this.factory.CreateDbContext();
+
+        context.Buckets.UpdateRange(buckets);
+        return context.SaveChangesAsync(cancellationToken);
     }
 }
