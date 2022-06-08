e4f0b31da8c229f9c8a2a5fe153802cce34ea1c0
Hannah Chan <hannah.chan@outlook.com>
2022-06-08 12:50:11 +0000

    Scaffold Unit Tests

---
+++ Scaffold/Tests/UnitTests/Adapters/Scaffold.Repositories.UnitTests/SingletonBucketRepositoryUnitTests.cs
@@ -0,0 +1,56 @@
+namespace Scaffold.Repositories.UnitTests;
+
+public class SingletonBucketRepositoryUnitTests
+{
+    public SingletonBucketRepositoryUnitTests()
+    {
+    }
+
+    public class AddBucket : SingletonBucketRepositoryUnitTests
+    {
+    }
+
+    public class AddBuckets : SingletonBucketRepositoryUnitTests
+    {
+    }
+
+    public class AddBucketAsync : SingletonBucketRepositoryUnitTests
+    {
+    }
+
+    public class AddBucketsAsync : SingletonBucketRepositoryUnitTests
+    {
+    }
+
+    public class RemoveBucket : SingletonBucketRepositoryUnitTests
+    {
+    }
+
+    public class RemoveBuckets : SingletonBucketRepositoryUnitTests
+    {
+    }
+
+    public class RemoveBucketAsync : SingletonBucketRepositoryUnitTests
+    {
+    }
+
+    public class RemoveBucketsAsync : SingletonBucketRepositoryUnitTests
+    {
+    }
+
+    public class UpdateBucket : SingletonBucketRepositoryUnitTests
+    {
+    }
+
+    public class UpdateBuckets : SingletonBucketRepositoryUnitTests
+    {
+    }
+
+    public class UpdateBucketAsync : SingletonBucketRepositoryUnitTests
+    {
+    }
+
+    public class UpdateBucketsAsync : SingletonBucketRepositoryUnitTests
+    {
+    }
+}
