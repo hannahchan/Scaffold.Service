ac149107ea71dba28040ad6f9393d44e13362dd8
Hannah Chan <hannah.chan@outlook.com>
2022-06-08 13:21:01 +0000

    Unit Test

--- Scaffold/Tests/UnitTests/Adapters/Scaffold.Repositories.UnitTests/SingletonBucketRepositoryUnitTests.cs
+++ Scaffold/Tests/UnitTests/Adapters/Scaffold.Repositories.UnitTests/SingletonBucketRepositoryUnitTests.cs
@@ -3,25 +3,44 @@ namespace Scaffold.Repositories.UnitTests;
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Scaffold.Domain.Aggregates.Bucket;
+using Xunit;
 
 public class SingletonBucketRepositoryUnitTests
 {
-    private readonly IDbContextFactory<BucketContext> bucketContextFactory;
-
-    public SingletonBucketRepositoryUnitTests()
+    private static ServiceProvider CreateNewServiceProvider()
     {
         ServiceCollection services = new ServiceCollection();
         services.AddDbContextFactory<BucketContext>(builder => builder.UseInMemoryDatabase(Guid.NewGuid().ToString()));
 
-        #pragma warning disable IDISP001 // Dispose created
-        ServiceProvider serviceProvider = services.BuildServiceProvider();
-        #pragma warning restore IDISP001 // Dispose created
-
-        this.bucketContextFactory = serviceProvider.GetRequiredService<IDbContextFactory<BucketContext>>();
+        return services.BuildServiceProvider();
     }
 
     public class AddBucket : SingletonBucketRepositoryUnitTests
     {
+        [Fact]
+        public void When_AddingBucket_Expect_Saved()
+        {
+            // Arrange
+            using ServiceProvider serviceProvider = CreateNewServiceProvider();
+
+            IDbContextFactory<BucketContext> contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<BucketContext>>();
+            SingletonBucketRepository repository = new SingletonBucketRepository(contextFactory);
+
+            Bucket bucket = new Bucket();
+
+            // Act
+            repository.Add(bucket);
+
+            // Assert
+            using BucketContext context = contextFactory.CreateDbContext();
+
+            Assert.Single(context.Buckets);
+
+            Bucket result = context.Buckets.Find(bucket.Id);
+            Assert.NotEqual(bucket, result);
+            Assert.Equal(bucket.Id, result.Id);
+        }
     }
 
     public class AddBuckets : SingletonBucketRepositoryUnitTests
