3eb4df2a7cd3c73a5a61806e845bdc632a900103
Hannah Chan <hannah.chan@outlook.com>
2022-06-09 12:23:35 +0000

    Unit Tests

--- Scaffold/Tests/UnitTests/Adapters/Scaffold.Repositories.UnitTests/SingletonBucketRepositoryUnitTests.cs
+++ Scaffold/Tests/UnitTests/Adapters/Scaffold.Repositories.UnitTests/SingletonBucketRepositoryUnitTests.cs
@@ -175,18 +175,262 @@ public class SingletonBucketRepositoryUnitTests
 
     public class RemoveBucket : SingletonBucketRepositoryUnitTests
     {
+        [Fact]
+        public void When_RemovingExistingBucket_Expect_Removed()
+        {
+            // Arrange
+            using ServiceProvider serviceProvider = CreateNewServiceProvider();
+
+            IDbContextFactory<BucketContext> contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<BucketContext>>();
+            SingletonBucketRepository repository = new SingletonBucketRepository(contextFactory);
+
+            Bucket[] buckets =
+            {
+                new Bucket { Name = "Bucket 1" },
+                new Bucket { Name = "Bucket 2" },
+                new Bucket { Name = "Bucket 3" },
+                new Bucket { Name = "Bucket 4" },
+                new Bucket { Name = "Bucket 5" },
+            };
+
+            using (BucketContext context = contextFactory.CreateDbContext())
+            {
+                context.Buckets.AddRange(buckets);
+                context.SaveChanges();
+            }
+
+            // Act
+            repository.Remove(buckets[2]);
+
+            // Assert
+            using (BucketContext context = contextFactory.CreateDbContext())
+            {
+                Assert.Null(context.Buckets.Find(buckets[2].Id));
+
+                Assert.Collection(
+                    context.Buckets,
+                    bucket => Assert.Equal("Bucket 1", bucket.Name),
+                    bucket => Assert.Equal("Bucket 2", bucket.Name),
+                    bucket => Assert.Equal("Bucket 4", bucket.Name),
+                    bucket => Assert.Equal("Bucket 5", bucket.Name));
+            }
+        }
     }
 
     public class RemoveBuckets : SingletonBucketRepositoryUnitTests
     {
+        [Fact]
+        public void When_RemovingEnumerableOfBuckets_Expect_Removed()
+        {
+            // Arrange
+            using ServiceProvider serviceProvider = CreateNewServiceProvider();
+
+            IDbContextFactory<BucketContext> contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<BucketContext>>();
+            SingletonBucketRepository repository = new SingletonBucketRepository(contextFactory);
+
+            Bucket[] buckets =
+            {
+                new Bucket { Name = "Bucket 1" },
+                new Bucket { Name = "Bucket 2" },
+                new Bucket { Name = "Bucket 3" },
+                new Bucket { Name = "Bucket 4" },
+                new Bucket { Name = "Bucket 5" },
+            };
+
+            using (BucketContext context = contextFactory.CreateDbContext())
+            {
+                context.Buckets.AddRange(buckets);
+                context.SaveChanges();
+            }
+
+            // Act
+            repository.Remove(new Bucket[] { buckets[1], buckets[3] });
+
+            // Assert
+            using (BucketContext context = contextFactory.CreateDbContext())
+            {
+                Assert.Null(context.Buckets.Find(buckets[1].Id));
+                Assert.Null(context.Buckets.Find(buckets[3].Id));
+
+                Assert.Collection(
+                    context.Buckets,
+                    bucket => Assert.Equal("Bucket 1", bucket.Name),
+                    bucket => Assert.Equal("Bucket 3", bucket.Name),
+                    bucket => Assert.Equal("Bucket 5", bucket.Name));
+            }
+        }
+
+        [Fact]
+        public void When_RemovingEmptyEnumerableOfBuckets_Expect_NoChange()
+        {
+            // Arrange
+            using ServiceProvider serviceProvider = CreateNewServiceProvider();
+
+            IDbContextFactory<BucketContext> contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<BucketContext>>();
+            SingletonBucketRepository repository = new SingletonBucketRepository(contextFactory);
+
+            Bucket[] buckets =
+            {
+                new Bucket { Name = "Bucket 1" },
+                new Bucket { Name = "Bucket 2" },
+                new Bucket { Name = "Bucket 3" },
+                new Bucket { Name = "Bucket 4" },
+                new Bucket { Name = "Bucket 5" },
+            };
+
+            using (BucketContext context = contextFactory.CreateDbContext())
+            {
+                context.Buckets.AddRange(buckets);
+                context.SaveChanges();
+            }
+
+            // Act
+            Exception exception = Record.Exception(() => repository.Remove(Array.Empty<Bucket>()));
+
+            // Assert
+            Assert.Null(exception);
+
+            using (BucketContext context = contextFactory.CreateDbContext())
+            {
+                Assert.Collection(
+                    context.Buckets,
+                    bucket => Assert.Equal("Bucket 1", bucket.Name),
+                    bucket => Assert.Equal("Bucket 2", bucket.Name),
+                    bucket => Assert.Equal("Bucket 3", bucket.Name),
+                    bucket => Assert.Equal("Bucket 4", bucket.Name),
+                    bucket => Assert.Equal("Bucket 5", bucket.Name));
+            }
+        }
     }
 
     public class RemoveBucketAsync : SingletonBucketRepositoryUnitTests
     {
+        [Fact]
+        public async Task When_RemovingExistingBucket_Expect_Removed()
+        {
+            // Arrange
+            using ServiceProvider serviceProvider = CreateNewServiceProvider();
+
+            IDbContextFactory<BucketContext> contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<BucketContext>>();
+            SingletonBucketRepository repository = new SingletonBucketRepository(contextFactory);
+
+            Bucket[] buckets =
+            {
+                new Bucket { Name = "Bucket 1" },
+                new Bucket { Name = "Bucket 2" },
+                new Bucket { Name = "Bucket 3" },
+                new Bucket { Name = "Bucket 4" },
+                new Bucket { Name = "Bucket 5" },
+            };
+
+            using (BucketContext context = contextFactory.CreateDbContext())
+            {
+                context.Buckets.AddRange(buckets);
+                context.SaveChanges();
+            }
+
+            // Act
+            await repository.RemoveAsync(buckets[2]);
+
+            // Assert
+            using (BucketContext context = contextFactory.CreateDbContext())
+            {
+                Assert.Null(context.Buckets.Find(buckets[2].Id));
+
+                Assert.Collection(
+                    context.Buckets,
+                    bucket => Assert.Equal("Bucket 1", bucket.Name),
+                    bucket => Assert.Equal("Bucket 2", bucket.Name),
+                    bucket => Assert.Equal("Bucket 4", bucket.Name),
+                    bucket => Assert.Equal("Bucket 5", bucket.Name));
+            }
+        }
     }
 
     public class RemoveBucketsAsync : SingletonBucketRepositoryUnitTests
     {
+        [Fact]
+        public async Task When_RemovingEnumerableOfBuckets_Expect_Removed()
+        {
+            // Arrange
+            using ServiceProvider serviceProvider = CreateNewServiceProvider();
+
+            IDbContextFactory<BucketContext> contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<BucketContext>>();
+            SingletonBucketRepository repository = new SingletonBucketRepository(contextFactory);
+
+            Bucket[] buckets =
+            {
+                new Bucket { Name = "Bucket 1" },
+                new Bucket { Name = "Bucket 2" },
+                new Bucket { Name = "Bucket 3" },
+                new Bucket { Name = "Bucket 4" },
+                new Bucket { Name = "Bucket 5" },
+            };
+
+            using (BucketContext context = contextFactory.CreateDbContext())
+            {
+                context.Buckets.AddRange(buckets);
+                context.SaveChanges();
+            }
+
+            // Act
+            await repository.RemoveAsync(new Bucket[] { buckets[1], buckets[3] });
+
+            // Assert
+            using (BucketContext context = contextFactory.CreateDbContext())
+            {
+                Assert.Null(context.Buckets.Find(buckets[1].Id));
+                Assert.Null(context.Buckets.Find(buckets[3].Id));
+
+                Assert.Collection(
+                    context.Buckets,
+                    bucket => Assert.Equal("Bucket 1", bucket.Name),
+                    bucket => Assert.Equal("Bucket 3", bucket.Name),
+                    bucket => Assert.Equal("Bucket 5", bucket.Name));
+            }
+        }
+
+        [Fact]
+        public async Task When_RemovingEmptyEnumerableOfBuckets_Expect_NoChange()
+        {
+            // Arrange
+            using ServiceProvider serviceProvider = CreateNewServiceProvider();
+
+            IDbContextFactory<BucketContext> contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<BucketContext>>();
+            SingletonBucketRepository repository = new SingletonBucketRepository(contextFactory);
+
+            Bucket[] buckets =
+            {
+                new Bucket { Name = "Bucket 1" },
+                new Bucket { Name = "Bucket 2" },
+                new Bucket { Name = "Bucket 3" },
+                new Bucket { Name = "Bucket 4" },
+                new Bucket { Name = "Bucket 5" },
+            };
+
+            using (BucketContext context = contextFactory.CreateDbContext())
+            {
+                context.Buckets.AddRange(buckets);
+                context.SaveChanges();
+            }
+
+            // Act
+            Exception exception = await Record.ExceptionAsync(() => repository.RemoveAsync(Array.Empty<Bucket>()));
+
+            // Assert
+            Assert.Null(exception);
+
+            using (BucketContext context = contextFactory.CreateDbContext())
+            {
+                Assert.Collection(
+                    context.Buckets,
+                    bucket => Assert.Equal("Bucket 1", bucket.Name),
+                    bucket => Assert.Equal("Bucket 2", bucket.Name),
+                    bucket => Assert.Equal("Bucket 3", bucket.Name),
+                    bucket => Assert.Equal("Bucket 4", bucket.Name),
+                    bucket => Assert.Equal("Bucket 5", bucket.Name));
+            }
+        }
     }
 
     public class UpdateBucket : SingletonBucketRepositoryUnitTests
